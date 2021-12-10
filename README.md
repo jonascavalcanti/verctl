956885e4afd68d7faf37d2ff6a06b137127f33fd
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2021-12-10 16:12:14 -0300

    Docs

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# xversioner
+# xversioner (CLI): xversioner controls application version
+
+## Using tool
+
+### Download dependencies
+````
+$ go mod tidy
+````
+### Normal Build
+````
+$ go build -v -o xversioner
+````
+### Build to Linux OS
+````
+$ env GOOS=linux GOARCH=amd64 go build -v -o xversioner
+````
+
+### Get aplication version example
+````
+$ xversioner get filepath ./app/file_contain_app_version
+or 
+
+$ xversioner get -f ./app/file_contain_app_version
+````
+
+### Update aplication version example
+````
+$ xversioner update filepath ./app/file_contain_app_version increment patch
+or
+$ xversioner update -f ./app/file_contain_app_version -i patch
+
+````
--- main.go
+++ main.go
@@ -4,22 +4,77 @@ import (
 	"fmt"
 	"os"
 	"xversioner/file"
-	"xversioner/utils"
+	"xversioner/help"
 )
 
-func main() {
-	fmt.Println("xVersioner Commands (CLI)")
+type Options struct {
+	Type     string
+	Filepath string
+}
 
-	if len(os.Args) < 2 || os.Args[1] == "--help" || os.Args[1] == "help" {
-		fmt.Println(utils.Help())
+func main() {
+
+	if len(os.Args) < 2 || os.Args[1] == "--help" || os.Args[1] == "-h" || os.Args[1] == "help" {
+		fmt.Println(help.Default())
 	}
 
-	oldVersion, b := file.GetVersion("./../app.properties")
-	fmt.Println("App Version:", oldVersion)
+	options := new(Options)
 
-	newVersion := file.IncrementVersion(oldVersion, ".", "minor")
+	for index, opt := range os.Args {
+
+		switch opt {
+		case "filepath", "-f":
+			if len(os.Args[index:]) <= 1 {
+				fmt.Println("You need to set the file path of application that contain the application version")
+				os.Exit(1)
+			} else {
+				options.Filepath = os.Args[index+1]
+			}
+		case "increment", "-i":
+			if len(os.Args[index:]) <= 1 {
+				fmt.Println("You need to define a increment type - (major | minor | path")
+				os.Exit(1)
+			} else {
+				options.Type = os.Args[index+1]
+			}
+		}
+	}
+
+	for index, opt := range os.Args {
+
+		switch opt {
+		case "update":
+			if len(os.Args[index:]) <= 1 || os.Args[index+1] == "--help" || os.Args[index+1] == "help" {
+				fmt.Println(help.Update())
+				os.Exit(1)
+			} else {
+				options.update()
+			}
+		case "get":
+			if len(os.Args[index:]) <= 1 || os.Args[index+1] == "--help" || os.Args[index+1] == "help" {
+				fmt.Println(help.Get())
+				os.Exit(1)
+			} else {
+				options.get()
+			}
+		}
+	}
+
+}
+
+func (opts Options) update() {
+
+	oldVersion, fileByteArray := file.GetVersion(opts.Filepath)
+	fmt.Println("Application Version:", oldVersion)
+
+	newVersion := file.IncrementVersion(oldVersion, ".", opts.Type)
 	fmt.Println("New Version:", newVersion)
 
-	file.WriteVerionOnFile(oldVersion, newVersion, b)
+	file.WriteVerionOnFile(oldVersion, newVersion, fileByteArray)
 
+}
+
+func (opts Options) get() {
+	version, _ := file.GetVersion(opts.Filepath)
+	fmt.Println("Application Version:", version)
 }
