f86ad416da92370d7eeefcf71db505cab81dbed6
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2021-12-28 13:59:12 -0300

    Generate date version like youtube-dl

--- file/file.go
+++ file/file.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //Read and return the content as string
@@ -21,16 +22,31 @@ func GetVersion(filepath string) (string, []byte) {
 	return string(version[1]), file
 }
 
-type Version struct {
+type SemVer struct {
 	Major int
 	Minor int
 	Patch int
 }
 
 func IncrementVersion(oldVersion, separator, typeInc string) string {
+
+	version := ""
+
+	if typeInc == "major" || typeInc == "minor" || typeInc == "patch" {
+		version = generateSemVer(oldVersion, separator, typeInc)
+	} else if typeInc == "date" {
+		version = generateDateVer(oldVersion, separator)
+	} else {
+		fmt.Println("Type", typeInc, "increment unavailable")
+	}
+
+	return version
+}
+
+func generateSemVer(oldVersion, separator, typeInc string) string {
 	arr := strings.Split(oldVersion, separator)
 
-	v := new(Version)
+	v := new(SemVer)
 
 	v.Major, _ = strconv.Atoi(arr[0])
 	v.Minor, _ = strconv.Atoi(arr[1])
@@ -40,13 +56,40 @@ func IncrementVersion(oldVersion, separator, typeInc string) string {
 		v.Major++
 	} else if typeInc == "minor" {
 		v.Minor++
-	} else {
+	} else if typeInc == "patch" {
 		v.Patch++
+	} else {
+		fmt.Println("Increment Type", typeInc, "unavailable ")
 	}
-	newVersion := strconv.Itoa(v.Major) + "." + strconv.Itoa(v.Minor) + "." + strconv.Itoa(v.Patch)
-	return newVersion
+
+	version := strconv.Itoa(v.Major) + "." + strconv.Itoa(v.Minor) + "." + strconv.Itoa(v.Patch)
+
+	return version
 }
 
+/**/
+func generateDateVer(oldVersion, separator string) string {
+	arr := strings.Split(oldVersion, separator)
+
+	fmt.Println(oldVersion)
+
+	layout := "2006.01.02"
+
+	t := time.Now()
+
+	dayInc, _ := (strconv.Atoi(arr[len(arr)-1]))
+
+	dayInc++
+
+	date := t.Format(layout)
+
+	version := date + "." + strconv.Itoa(dayInc)
+
+	return version
+
+}
+
+/**/
 func WriteVerionOnFile(filepath, oldVersion, newVersion string, file []byte) {
 	output := bytes.Replace(file, []byte(oldVersion), []byte(newVersion), -1)
 
